dac28cf925e694439de7e483004e566f932eb288
rexmacofficial <rexmacofficial@gmail.com>
2023-12-31 11:39:43 -0500

    added burger menu
    - created new js file
    - added new burger menu to list
    - link jquery across pages
    - added new items to css to allow for responsive burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Howdy partners");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
